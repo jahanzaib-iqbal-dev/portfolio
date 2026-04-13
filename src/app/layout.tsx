a09829941cb33528678dfdbb69f89903795be3c5
Jahanzaib iqbal <jahanzaib@gsoftconsulting.com>
2026-04-13 10:46:20 +0500

    feat: add Microsoft Clarity analytics (wawkqu4rhg)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -102,6 +102,19 @@ export default async function RootLayout({
             `,
           }}
         />
+        {/* Microsoft Clarity */}
+        <script
+          id="clarity-init"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wawkqu4rhg");
+            `,
+          }}
+        />
       </head>
       <Providers>
         <Column
